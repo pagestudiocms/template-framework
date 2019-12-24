dc87a2397c1065516eb58b326551b920e0395a5e
cosmomathieu <cosmo@cosmointeractive.co>
2019-12-24 12:14:56 -0500

    Added mobile overlay menu

--- sandbox/ui-kit/header/js/main.js
+++ sandbox/ui-kit/header/js/main.js
@@ -1,31 +1,66 @@
 ;(function () {
 	
 	'use strict';
-
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-			BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-			Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-			any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
-
-	$(function(){
-		$('.navbar-collapse').on('shown.bs.collapse', function(e) {
-      //  Do nothing... menu is open 
-		})
+	
+	/**
+	 * Detect the current active responsive breakpoint in Bootstrap
+	 * @returns {string}
+	 * @author farside {@link https://stackoverflow.com/users/4348232/hckr}
+	 * @see https://stackoverflow.com/a/39552462
+	 */
+	function getBootstrapBreakpoint(){
+	  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	  return (w < 768) ? 'xs' : ((w < 992) ? 'sm' : ((w < 1200) ? 'md' : 'lg'));
+	}
+  
+	function initMobileNavOverlay(){
+	  var $mobileNavToggler = $('#navbar-nav-toggle');
+	  let breakpoint = getBootstrapBreakpoint();
+	  let targetOriginal = $mobileNavToggler[0].dataset.targetOriginal;
+	  switch(breakpoint){
+		case 'xs':
+		  $mobileNavToggler[0].dataset.target = `#${targetOriginal}-overlay`;
+		  $mobileNavToggler[0].dataset.toggle = `modal`;
+		  initMobileNavOverlayEvent(targetOriginal);
+		break;
+		default:
+		  $mobileNavToggler[0].dataset.target = `#${targetOriginal}`;
+		  $mobileNavToggler[0].dataset.toggle = 'collapse';
+	  }
+	}
+  
+	function initMobileNavOverlayEvent(el){
+	  if(typeof window.initMobileOverlay === 'undefined'){
+		$(`#${el}-overlay`).on('show.bs.modal', function(e){
+		  var target = e.currentTarget;
+		  var $modalContent = $(target).find('.mobile-nav-body');
+		  const excludes = ['navbar-left', 'navbar-right'];
+  
+		  if(typeof $modalContent.find('.navbar-nav')[0] === 'undefined'){
+			var $topNav = $(`#${el}`);
+			var list = $topNav.clone();
+			list.children().each(function(k,el){
+			  el.classList.remove(...excludes);
+			  $(target).find('.mobile-nav-body').append(el);
+			});
+		  }
+  
+		  $('#navbar-nav-toggle').removeClass('collapsed');
+		  $('body').addClass('mobile-overlay-visible');
+		});
+		$(`#${el}-overlay`).on('hide.bs.modal', function(e){
+		  $('#navbar-nav-toggle').addClass('collapsed');
+		  $('body').removeClass('mobile-overlay-visible'); 
+		});
+		window.initMobileOverlay = true;
+	  }
+	}
+  
+	$(window).resize(function(){
+	  initMobileNavOverlay();
 	});
+  
+  $(document).ready(function(){
+	  initMobileNavOverlay();
+  });
 }());
