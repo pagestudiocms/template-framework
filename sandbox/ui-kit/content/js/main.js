0038b0c08d9119f8ec58117b15a4cdf4d71da8b9
cosmomathieu <cosmo@cosmointeractive.co>
2019-12-24 13:18:44 -0500

    Added flexslider styles to ui kit

---
+++ sandbox/ui-kit/content/js/main.js
@@ -0,0 +1,16 @@
+var flexSlider = function() {
+  $('.flexslider').flexslider({
+    animation: "fade",
+    prevText: "",
+    nextText: "",
+    slideshow: true
+  });
+}
+
+$(window).load(function(){
+  flexSlider();
+});
+
+$(document).ready(function(){
+
+});
